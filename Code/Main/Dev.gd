c1460e05035821570bfc07eefe310209475c3a60
unblinky <12799346-unblinky@users.noreply.gitlab.com>
2024-03-12 13:17:37 -0700

    Finished?

--- Code/Main/Dev.gd
+++ Code/Main/Dev.gd
@@ -2,14 +2,14 @@ extends Node
 class_name Dev
 
 static func RemoveChildrenOf(node: Node):
-  for n in node.get_children():
-    node.remove_child(n)
-    n.queue_free()
+	for n in node.get_children():
+		node.remove_child(n)
+		n.queue_free()
 
 
 static func RandomPointOnCircle(radius: float) -> Vector2:
-  var angle: float = randf_range(0.0, PI * 2.0)
-  return Vector2(sin(angle) * radius, cos(angle) * radius)
+	var angle: float = randf_range(0.0, PI * 2.0)
+	return Vector2(sin(angle) * radius, cos(angle) * radius)
   
 # var x: float = sin(angle) * radius
 # var y: float = cos(angle) * radius
